3e2e3a18ab9f48145190747f1da40a00a48b2b57
AdalricP <78260488+AdalricP@users.noreply.github.com>
2025-08-25 05:24:39 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,27 +2,31 @@
 
 int main(void){
     char raw[] = "AABBCCDCCDDBBA";
-    char RLE[100];
+    char RLE[20] = {0}; 
     char current = raw[0];
-
     int count = 1; 
-    for(int i = 1; raw[i] != '\0'; i++){
+    int RLE_index = 0;   
 
-        int RLE_index = 0;
+    for(int i = 1; raw[i] != '\0'; i++){
         if(current==raw[i]){
             count++;
         } else {
-            printf("%c", current);
-            printf("%d", count);
-            RLE[RLE_index] = count;
-            RLE_index++;
             RLE[RLE_index] = current;
+               
+            RLE_index++;
+            RLE[RLE_index] = count + '0'; 
             RLE_index++;
             current = raw[i];
             count=1;
         }
     }
-    
 
+    
+    RLE[RLE_index] = current;
+    RLE_index++;
+    RLE[RLE_index] = count + '0';
+    RLE[RLE_index + 1] = '\0';             
+
+    printf("%s\n", RLE);
     return 0;
 }
